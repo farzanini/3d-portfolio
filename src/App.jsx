f494ef31152e28274f993645e3cf751c9d1da0f1
Farzan <farzan.hosseiny@gmail.com>
2023-07-21 15:37:42 +0330

    fixing stars position

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className="relateive z-0">
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
