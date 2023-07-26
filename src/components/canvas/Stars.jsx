17a2c927dc158cff83f66e1a46cc3825cae5da78
Farzan <farzan.hosseiny@gmail.com>
2023-07-26 16:47:38 +0330

    now below 1000 !

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = random.inSphere(new Float32Array(1500), { radius: 1.2 });
+  const sphere = random.inSphere(new Float32Array(900), { radius: 1.2 });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
