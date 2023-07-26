12702f7cbb713983b4df30d6342fb397fc6ab921
Farzan <farzan.hosseiny@gmail.com>
2023-07-26 22:37:23 +0330

    no star component

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,35 +1,12 @@
-import { useState, useRef, Suspense, useEffect } from "react";
+import { useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [isMobile, setIsMobile] = useState(false);
 
-  const sphere = !isMobile
-    ? random.inSphere(new Float32Array(5000), { radius: 1.2 })
-    : random.inSphere(new Float32Array(1), { radius: 1.2 });
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
+  const sphere = random.inSphere(new Float32Array(3000), { radius: 1.2 });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
--- src/constants/index.js
+++ src/constants/index.js
@@ -77,34 +77,34 @@ const technologies = [
     name: "Redux Toolkit",
     icon: redux,
   },
-  // {
-  //   name: "Tailwind CSS",
-  //   icon: tailwind,
-  // },
-  // {
-  //   name: "Python",
-  //   icon: python,
-  // },
-  // {
-  //   name: "Django",
-  //   icon: django,
-  // },
-  // {
-  //   name: "Three JS",
-  //   icon: threejs,
-  // },
-  // {
-  //   name: "git",
-  //   icon: git,
-  // },
-  // {
-  //   name: "figma",
-  //   icon: figma,
-  // },
-  // {
-  //   name: "docker",
-  //   icon: docker,
-  // },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "Django",
+    icon: django,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
 ];
 
 const experiences = [
